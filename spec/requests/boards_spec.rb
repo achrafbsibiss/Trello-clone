cfb71327fe79e85e3d90602fa8f9034a6809ed47
achrafbsibiss <achraf.bsibiss11@gmail.com>
2023-07-22 15:19:27 +0100

    Working in the creat Board and the spec

--- spec/requests/boards_spec.rb
+++ spec/requests/boards_spec.rb
@@ -1,16 +1,61 @@
 require 'rails_helper'
 
-RSpec.describe "Boards", type: :request do
+RSpec.describe 'Boards', type: :request do
   let(:user) { create(:user) }
+  let(:valid_attributes) do
+    {
+      name: 'New Title'
+    }
+  end
+
+  let(:invalid_attributes) do
+    {
+      name: ''
+    }
+  end
 
   before do
     sign_in user
   end
 
-  describe "GET /new" do
+  describe 'GET /new' do
     it 'renders a successful response' do
       get new_board_path
       expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe 'POST /creat' do
+    context 'with valid params' do
+      before do
+        post boards_path, params: {board: valid_attributes}
+      end
+
+      it 'redirect to dashboard' do
+        expect(response).to have_http_status(:redirect)
+      end
+
+      it 'creates a new announcement' do
+        expect(Board.count).to eq(1)
+      end
+    end
+  end
+
+  context 'with valid params' do
+    before do
+      post boards_path, params: {board: invalid_attributes}
+    end
+
+    it 'redirect to dashboard' do
+      redirect_to root_path
+    end
+
+    it 'creates a new announcement' do
+      expect(Board.count).to eq(0)
+    end
+
+    it 'returns an unprocessable entity status' do
+      expect(response).to  have_http_status(:success)
     end
   end
 end
